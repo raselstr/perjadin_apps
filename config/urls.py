109175248b488efedf8fb495853e24405101872f
Rasel S Str <str.rasel29@gmail.com>
2026-04-06 00:15:39 +0700

    admin sudah masuk

--- config/urls.py
+++ config/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 from dashboard.views import home_redirect
 from django.contrib.auth import views as auth_views
+from django.contrib import admin............................
 
 urlpatterns = [
-    path('', home_redirect),  # 👈 ROOT diarahkan ke sini
+    path('admin/', admin.site.urls),  # 🔥 INI WAJIB ADA
 
-    path('login/', auth_views.LoginView.as_view(template_name='auth/login.html'), name='login'),
+    path('', home_redirect),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/login/'), name='logout'),
 
     path('dashboard/', include('dashboard.urls')),
