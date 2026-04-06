b5c73782df5189b8de5fad3cb5126b70a952c698
Rasel S Str <54805845+raselstr@users.noreply.github.com>
2026-04-06 10:40:56 +0700

    login

--- config/urls.py
+++ config/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),  # 🔥 INI WAJIB ADA
 
     path('', home_redirect),
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='auth/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/login/'), name='logout'),
 
     path('dashboard/', include('dashboard.urls')),
