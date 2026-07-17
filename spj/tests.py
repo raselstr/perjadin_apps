9a915201c9dc07855f8132b3df0d6c94e8f5e354
raselstr <str.rasel29@gmail.com>
2026-07-17 15:00:05 +0700

    upload bukti dan pdf spt

--- spj/tests.py
+++ spj/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+from django.template.loader import render_to_string
+from django.test import SimpleTestCase
 
-# Create your tests here.
+
+class SPJUploadTemplateTests(SimpleTestCase):
+    def test_photo_handler_skips_non_image_files(self):
+        template = render_to_string("includes/scripts.html", request=None)
+
+        self.assertIn("shouldHandleInput(input)", template)
+        self.assertIn("file.type && file.type.startsWith('image/')", template)
+        self.assertNotIn("accept.includes('image')", template)
